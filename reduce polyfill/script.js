a2241894ec638262b49fff44da4665675982b5e9
Srivarsha <srivarsha.chirla@gmail.com>
2024-10-20 12:29:02 +0530

    polyfills commit

---
+++ reduce polyfill/script.js
@@ -0,0 +1,12 @@
+Array.prototype.myReduce=function(cb,initialValue){
+    var accumulator=initialValue;
+    for(let i=0;i<this.length;i++){
+        accumulator=accumulator?cb(accumulator,this[i],i,this):this[i];
+    }
+    return accumulator;
+};
+const nums=[1,2,3,4];
+const sum=nums.myReduce((acc,curr,i,arr)=>{
+    return acc+curr;
+},0);
+console.log(sum);
